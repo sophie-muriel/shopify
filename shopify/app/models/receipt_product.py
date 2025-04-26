d7bae8f92e006ef53dea840f8a38bbd739c4cab3
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 16:57:51 -0500

    feat(receipt_product.py): add attributes

---
+++ shopify/app/models/receipt_product.py
@@ -0,0 +1,13 @@
+# models/receipt_product.py
+from sqlalchemy import Column, Integer, ForeignKey
+from db.database import Base
+from models.product import Product
+from models.receipt import Receipt
+
+
+class Receipt_product(Base):
+    __tablename__ = "receipt_product"
+
+    id = Column(Integer, primary_key=True, index=True)
+    receipt_id = Column(Integer, ForeignKey(Receipt.id))
+    product_id = Column(Integer, ForeignKey(Product.id))
