ed1ff70d09b1e1f10c448676d6e908f25dcddf04
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 16:37:15 -0500

    feat(receipt.py): add proper attributes

--- shopify/app/models/receipt.py
+++ shopify/app/models/receipt.py
@@ -7,8 +7,8 @@ class Receipt(Base):
     __tablename__ = "receipt"
 
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(String)
-    client_name = Column(String)
-    client_email = Column(String)
+    date = Column(String(10))
+    client_name = Column(String(40))
+    client_email = Column(String(30))
     total_price = Column(Integer)
 
