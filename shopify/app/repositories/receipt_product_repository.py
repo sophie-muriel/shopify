d195634f08a5c5f7311cb1724ef8ef0c27681dfb
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 18:24:59 -0500

    feat: create receipt_product_repository.py

---
+++ shopify/app/repositories/receipt_product_repository.py
@@ -0,0 +1,25 @@
+# repositories/receipt_repository.py
+from sqlalchemy.orm import Session
+from models.receipt_product import Receipt_product
+from schemas.receipt import ReceiptProductCreate
+
+
+def create_receipt_product(db: Session, receipt_product: ReceiptProductCreate):
+    db_receipt_product = Receipt_product(**receipt_product.dict())
+    db.add(db_receipt_product)
+    db.commit()
+    db.refresh(db_receipt_product)
+    return db_receipt_product
+
+
+def get_receipt_product(db: Session, receipt_product_id: int):
+    return db.query(Receipt_product).filter(Receipt_product.id == receipt_product_id).first()
+
+
+def delete_receipt(db: Session, receipt_id: int):
+    db_receipt = get_receipt(db, receipt_id)
+    if not db_receipt:
+        return None
+    db.delete(db_receipt)
+    db.commit()
+    return {"message": "receipt_product deleted successfully"}
