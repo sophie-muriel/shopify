6ace8367b97bb65f7786ff70be928aed395463a9
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 18:08:57 -0500

    feat: create product_repository.py

---
+++ shopify/app/repositories/product_repository.py
@@ -0,0 +1,37 @@
+# repositories/product_repository.py
+from sqlalchemy.orm import Session
+from models.product import Product
+from schemas.product import ProductCreate
+
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def get_product(db: Session, product_id: int):
+    return db.query(Product).filter(Product.id == product_id).first()
+
+
+def update_product(db: Session, product_id: int, product: ProductCreate):
+   db_product = get_product(db, product_id)
+   if not db_product:
+       return None
+   db_product.product_name = product.product_name
+   db_product.description = product.description
+   db_product.price = product.price
+   db.commit()
+   db.refresh(db_product)
+   return db_product
+
+
+def delete_product(db: Session, product_id: int):
+    db_product = get_product(db, product_id)
+    if not db_product:
+        return None
+    db.delete(db_product)
+    db.commit()
+    return {"message": "product deleted successfully"}
