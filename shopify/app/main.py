928f26bff87e9a1b963368632b281732d605ea0a
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 20:29:42 -0500

    feat: update main.py

---
+++ shopify/app/main.py
@@ -0,0 +1,40 @@
+# main.py
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+import os
+
+from app.api.v1 import product_ruotes
+
+# Database configuration
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", ""
+)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app = FastAPI(
+    root_path="/api",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+    swagger_ui_parameters={
+        "defaultModelsExpandDepth": -1,
+        "persistAuthorization": True,
+    },
+)
+
+app.include_router(product_ruotes.router, prefix="/v1/product", tags=["Products"])
