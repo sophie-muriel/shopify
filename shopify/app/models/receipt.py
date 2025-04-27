4b19217be72ff7ebda7dd85f6f6b06fc798a5b53
Sophie <miso.png@gmail.com>
2025-04-26 23:44:46 -0500

    style(receipt.py): remove white spaces in receipt model and modify data types

--- shopify/app/models/receipt.py
+++ shopify/app/models/receipt.py
@@ -1,5 +1,5 @@
 # models/receipt.py
-from sqlalchemy import Column, Integer, String, Double
+from sqlalchemy import Column, Integer, String, Date
 from ..db.database import Base
 
 
@@ -7,7 +7,6 @@ class Receipt(Base):
     __tablename__ = "receipt"
 
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(String(10))
+    date = Column(Date)
     client_name = Column(String(40))
-    client_email = Column(String(30))
-
+    client_email = Column(String(255))
