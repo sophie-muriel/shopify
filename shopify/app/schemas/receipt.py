07f02430b5134ab61b40a82b922ba82b300de9f5
Sophie <miso.png@gmail.com>
2025-04-26 23:47:07 -0500

    style(receipt.py): remove white spaces and add extra line between imports and first function in receipt schema

--- shopify/app/schemas/receipt.py
+++ shopify/app/schemas/receipt.py
@@ -2,21 +2,19 @@
 from pydantic import BaseModel
 from typing import List
 
+
 class ReceiptProductCreate(BaseModel):
     receipt_id: int
     product_id: int
     quantity: int
-
 
 class ReceiptBase(BaseModel):
     date: str
     client_name: str
     client_email: str
 
-
 class ReceiptCreate(ReceiptBase):
     products_list: List['ReceiptProductCreate']
-
 
 class ReceiptResponse(ReceiptBase):
     id: int
@@ -24,5 +22,3 @@ class ReceiptResponse(ReceiptBase):
 
     class Config:
         orm_mode = True
-
-    
