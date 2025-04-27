229075c736744c35d7c180244697d9f40db0a526
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-27 18:43:29 -0500

    feat(owner_schema.py): add some field validations

--- shopify/app/schemas/owner_schema.py
+++ shopify/app/schemas/owner_schema.py
@@ -1,12 +1,12 @@
 # schemas/owner_schema.py
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, constr
 
 
 class OwnerBase(BaseModel):
     first_name: str
     last_name: str
-    phone: str
-    email: str
+    phone: str = constr(min_length=10, max_length=10)
+    email: EmailStr
     address: str
 
 
