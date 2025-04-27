a5b89df75d9858efda66ed22d1deaadbba515c7b
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-26 19:03:31 -0500

    refactor(product_service): ubdate function names

--- shopify/app/services/product_service.py
+++ shopify/app/services/product_service.py
@@ -1,4 +1,4 @@
-# services/item_service.py
+# services/product_service.py
 from sqlalchemy.orm import Session
 from repositories.product_repository import create_product, get_product, update_product, delete_product
 from schemas.product import ProductCreate
@@ -16,5 +16,5 @@ def update_existing_product(db: Session, product_id: int, product: ProductCreate
    return update_product(db, product_id, product)
 
 
-def delete_existing_item(db: Session, product_id: int):
+def delete_existing_product(db: Session, product_id: int):
     return delete_product(db, product_id)
