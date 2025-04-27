fb67b2bd830cb1bc915281f92c0d0c1e1cb98f6d
Sophie <miso.png@gmail.com>
2025-04-27 03:41:12 -0500

    fix(receipt_schema): change product list from product response to receipt x product response

--- shopify/app/schemas/receipt_schema.py
+++ shopify/app/schemas/receipt_schema.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List
 from datetime import date
 from ..schemas.product_schema import ProductResponse
+from ..schemas.receipt_product_schema import ReceiptProductResponse
 
 
 class ReceiptProductCreate(BaseModel):
@@ -23,7 +24,7 @@ class ReceiptCreate(ReceiptBase):
 class ReceiptResponse(ReceiptBase):
     id: int
     total_price: float
-    products: List[ProductResponse]
+    products: List[ReceiptProductResponse]
 
     class Config:
         orm_mode = True
