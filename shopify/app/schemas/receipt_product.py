5a079e03c9ce8b90571554e6a49932b8cbbaaca5
Sophie <miso.png@gmail.com>
2025-04-26 23:48:22 -0500

    feat(receipt_product.py): create schema for receipt_product entity

---
+++ shopify/app/schemas/receipt_product.py
@@ -0,0 +1,17 @@
+# schemas/receipt_product.py
+from pydantic import BaseModel
+
+
+class ReceiptProductBase(BaseModel):
+    receipt_id: int
+    product_id: int
+    quantity: int
+
+class ReceiptProductCreate(ReceiptProductBase):
+    pass
+
+class ReceiptProductResponse(ReceiptProductBase):
+    id: int
+
+    class Config:
+        orm_mode = True
