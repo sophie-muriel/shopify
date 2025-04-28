99edc65c185ef0902fb5108932b9d53eef1c7d0c
Sophie <miso.png@gmail.com>
2025-04-27 20:58:19 -0500

    feat(SexEnum): move SexEnum to commons folder and adjust imports in pet model and schema

---
+++ shopify/app/commons/enums.py
@@ -0,0 +1,13 @@
+# common/enums.py
+from enum import Enum
+
+
+class SexEnum(Enum):
+    """
+    Enum representing the sex of a pet.
+    Attributes:
+    male: Represents male sex.
+    female: Represents female sex.
+    """
+    male = "male"
+    female = "female"
--- shopify/app/schemas/pet_schema.py
+++ shopify/app/schemas/pet_schema.py
@@ -1,18 +1,6 @@
 # schemas/pet_schema.py
 from pydantic import BaseModel, Field, conint, constr
-from enum import Enum as PyEnum
-
-
-class SexEnum(PyEnum):
-    """
-    Enum representing the sex of a pet.
-
-    Attributes:
-        male: Represents male sex.
-        female: Represents female sex.
-    """
-    male = "Male"
-    female = "Female"
+from ..commons.enums import SexEnum
 
 
 class PetBase(BaseModel):
@@ -23,7 +11,7 @@ class PetBase(BaseModel):
         name (str): The name of the pet. This is a required field and cannot be empty.
         species (str): The species of the pet. This is a required field and cannot be empty.
         breed (str | None): The breed of the pet. This field is optional and can be None.
-        sex (SexEnum): The sex of the pet, must be either 'Male' or 'Female'.
+        sex (SexEnum): The sex of the pet, must be either 'male' or 'female'.
         owner_id (int): The unique identifier of the pet's owner. This field is required.
     """
     name: str = constr(min_length=1, max_length=100)
