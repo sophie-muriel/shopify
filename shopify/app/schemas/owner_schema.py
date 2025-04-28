9473b68dbf041e204901c4d1edef2a257b5e11df
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-27 21:15:05 -0500

    docs(owner_schema.py): add OwnerBase class documentation

--- shopify/app/schemas/owner_schema.py
+++ shopify/app/schemas/owner_schema.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel, EmailStr, constr
 
 
 class OwnerBase(BaseModel):
+    """
+    Base schema for creating or updating an owner.
+
+    Attributes:
+        first_name (str): The first name of the owner. This is a required field and cannot be empty.
+        last_name (str): The last name of the owner. This is a required field and cannot be empty.
+        phone (str): The phone number of the owner. It must be exactly 10 characters long.
+        email (EmailStr): The email address of the owner. This is a required field and must follow a valid email format.
+        address (str | None): The address of the owner. This field is optional and can be None.
+  """
     first_name: str
     last_name: str
     phone: str = constr(min_length=10, max_length=10)
