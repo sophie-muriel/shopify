04b558c85cc512a7962a1992cc03df3a45c7f58d
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-04-27 21:14:44 -0500

    docs(owner.py): add Owner class documentation

--- shopify/app/models/owner.py
+++ shopify/app/models/owner.py
@@ -5,6 +5,20 @@ from ..db.database import Base
 
 
 class Owner(Base):
+    """
+    Represents an owner in the system.
+
+    Attributes:
+        id (int): The unique identifier for the owner.
+        first_name (str): The first name of the owner. This is a required field.
+        last_name (str): The last name of the owner. This is a required field.
+        phone (str): The phone number of the owner. Optional field.
+        email (str): The email address of the owner. This is a required field.
+        address (str): The address of the owner. Optional field.
+
+    Relationships:
+        pets (Pet): The pets owned by the owner. It links to the `Pet` model.
+  """
     __tablename__ = "owners"
 
     id = Column(Integer, primary_key=True, index=True)
